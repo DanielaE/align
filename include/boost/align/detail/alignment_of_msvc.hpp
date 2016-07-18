9a171881116c791a6621468c6447b8a217749e0c
Daniela Engert <dani@ngrt.de>
2016-07-18 15:02:23 +0200

    silence msvc compiler warnings about structure member alignment
    Signed-off-by: Daniela Engert <dani@ngrt.de>
    (cherry picked from commit 2d6c866ed203f7143fed1d62068179c2a1686133)

--- include/boost/align/detail/alignment_of_msvc.hpp
+++ include/boost/align/detail/alignment_of_msvc.hpp
@@ -8,6 +8,9 @@ Distributed under the Boost Software License, Version 1.0.
 #ifndef BOOST_ALIGN_DETAIL_ALIGNMENT_OF_MSVC_HPP
 #define BOOST_ALIGN_DETAIL_ALIGNMENT_OF_MSVC_HPP
 
+#pragma warning(push)
+#pragma warning(disable: 4121) // alignment of a member was sensitive to packing
+
 #include <boost/align/detail/min_size.hpp>
 
 namespace boost {
@@ -30,4 +33,5 @@ struct alignment_of
 } /* alignment */
 } /* boost */
 
+#pragma warning(pop)
 #endif
